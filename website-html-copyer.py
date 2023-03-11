a5d3577c423705097932dec9d8ec329a34c27a08
daboi-gonna-kill-u <98598957+daboi-gonna-kill-u@users.noreply.github.com>
2023-03-11 12:06:01 +0530

    more efficient and has the ability to put code in your selected file

--- website-html-copyer.py
+++ website-html-copyer.py
@@ -1,17 +1,32 @@
+#this code will download the html code of the entered link
+#inputing link and file name to store file in
+from requests_html import HTMLSession
 import requests
-c = []
-a = input('enter your link: ')
-for i in range(len(a)):
-    c.append(a[i])
+link = input('Paste your link here: ')
+file_name = input('Enter the file or file location you want to store the webpage in (dont forget to add file attributes such as .html .exe .txt etc) :')
+#this code will check if the link entered is wrong and will correct it. eg if value is entered as 'google.com' it will automatically add 'https://'
+#we will split each character to check what the user has forgotten to put
+separated_link = link.split('.',maxsplit=(3))
 
-if c[0] != 'h' and c[2] != 't' and c[3] != 't' and c[4] != 'p' and c[5] != 's' and c[6] != ':' and c[7] != '/' and c[8] != '/':
-    a = 'https://'+ a
+#check missing characters and add them to link variable
 
-elif c[0] != 'h' and c[2] != 't' and c[3] != 't' and c[4] != 'p' and c[5] != 's' and c[6] != ':' and c[7] != '/' and c[8] != '/' and d[9] != 'w' and d[10] != 'w' and d[11] != 'w' and d[12] != '.':
-    a = "https://www."+ a
+if separated_link[0] == 'www':
+    link = 'https://'+ link
 
-b = requests.get(a)
-a = open('website_codes.txt','w')
-a.write(b.text)
+elif separated_link[0] != 'https://www.':
+    link = 'https://'+link
 
-print(b.text)
+#checking if user has forgot to put '.com' or it is intentional like '.in' or '.co' or '.net' or '.org'
+if separated_link[-1] != 'in' or separated_link[-1] != 'org' or separated_link[-1] != 'in' or separated_link[-1] != 'net' or separated_link[-1] != 'co':
+    if separated_link[-1] != '.com':
+        link = link + '.com'
+
+#requesting file
+file_value = requests.get(link)
+
+#saving file
+with open(str(file_name), 'w', encoding='utf-8') as website_file:
+    website_file.write(file_value.text)
+
+print(file_value.text)
+print(link)
